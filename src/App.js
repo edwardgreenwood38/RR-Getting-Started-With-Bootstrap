88cc80e1e21e3a80f6309a6fb6a017bdecc5834a
Edward E Greenwood <edwardgreenwood38@gmail.com>
2024-02-27 20:14:25 -0500

    step 4 completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
 import Button from 'react-bootstrap/Button';
+import Nav from 'react-bootstrap/Nav'
 
 
 function App() {
   return (
     <div className="App">
+      <Nav variant="tabs" activeKey="/home">
+        <Nav.Item>
+          <Nav.Link href="/home">Home</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="/about">About Us</Nav.Link>
+        </Nav.Item>
+    </Nav>
+
+
       <h1>Bootstrap Website</h1>
       <Button variant="outline-success">Run!!</Button>{' '}
     </div>
